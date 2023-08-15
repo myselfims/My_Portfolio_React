96febc563b0026918ada3c38fb75af2d076b2f81
myselfims <shaikhimran7585@gmail.com>
2023-08-16 03:20:17 +0530

    third commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,10 @@ import {GiHamburgerMenu} from 'react-icons/gi'
 
 const Header = ({handleJump}) => {
 
-    const [mobileNav, setMobileNav] = useState('hidden')
+    const [mobileNav, setMobileNav] = useState(false)
 
     const handleClick = (target) => {
-        setMobileNav('hidden')
+        setMobileNav(false)
         handleJump(target)
     }
     
@@ -19,7 +19,7 @@ const Header = ({handleJump}) => {
             <div className="logo z-10 max-sm:absolute max-sm:top-1 flex justify-center items-center">
                 <h1 className='text-3xl font-handWriting min-w-max font-bold bg-gradient-to-r from-primary to-danger bg-clip-text text-transparen'>Shaikh Imran</h1>
             </div>
-            <div className={`w-full max-sm:py-10 max-sm:${mobileNav} justify-between max-sm:flex-col`}>
+            <div className={`flex w-full max-sm:py-10 max-sm:${mobileNav?'flex':'hidden'} justify-between max-sm:flex-col`}>
                 <div className="pl-10">
                     <ul className='flex items-center max-sm:flex-col'>
                         <li onClick={()=>handleClick('about')} className='font-bold text-xl mx-5 cursor-pointer hover:text-sky-400 transition-all '>About</li>
@@ -38,7 +38,7 @@ const Header = ({handleJump}) => {
                 </div>
             </div>
             <div className='max-sm:inline absolute top-2 right-2 hidden'>
-                <GiHamburgerMenu onClick={()=>{mobileNav==='flex'?setMobileNav('hidden'):setMobileNav('flex')}} className={`w-9 cursor-pointer ${mobileNav==='flex'?'text-cyan-500':null} h-9`}/>
+                <GiHamburgerMenu onClick={()=>{mobileNav?setMobileNav(false):setMobileNav(true)}} className={`w-9 cursor-pointer ${mobileNav?'text-cyan-500':null} h-9`}/>
             </div>
             
         </nav>
